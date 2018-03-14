47174a680705ddc9039b85f431c381ecad439dca
Ahmed-Ramadan <ahmed_ramadanc@yahoo.com>
2018-03-14 20:54:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+##My portfolio
+_This is a project from udacity front-end nano degree_
---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(window).scroll(function () {
+		$("#nav").css("position","fixed");
+		$("#nav").addClass( "fff" );
+		$("#top-nav").css("top","0px");
+		$("#top-nav").css("position","fixed");
+		$("#top-nav").addClass("animation");
+
+	});
+
+});
+
